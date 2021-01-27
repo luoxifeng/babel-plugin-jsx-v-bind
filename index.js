08a035b0ea3d0a58fb0c1e19c08ca7a12b8855ee
luoxifeng <942394682@qq.com>
2021-01-27 15:00:26 +0800

    feat: opt code

--- index.js
+++ index.js
@@ -2,61 +2,51 @@
 
 
 module.exports = function (babel) {
-    var t = babel.types
+  var t = babel.types
 
-    return {
-        inherits: require('babel-plugin-syntax-jsx'),
-        visitor: {
-            JSXOpeningElement: function JSXOpeningElement(path) {
-                var bind = null;
-                var bindAttribute = null;
+  return {
+    inherits: require('babel-plugin-syntax-jsx'),
+    visitor: {
+      JSXOpeningElement: function JSXOpeningElement(path) {
+        var bind = null;
+        var bindAttribute = null;
 
-                path.get('attributes').forEach(function (path) {
-                    const name = path.node.name;
-                    const value = path.node.value;
-                    if (!name) {
-                        return
-                    }
+        path.get('attributes').forEach(function (path) {
+          const name = path.node.name;
+          const value = path.node.value;
+          if (!name) return
 
-                    console.log(path.node.value);
-                    const _name = name.name
-                    if (_name === 'v-bind' || _name === 'vBind') {
-                        bindAttribute = path
-                    } else {
-                        return
-                    }
-                    
-                    /**
-                     * 一个节点上不能使用多次v-bind
-                     */
-                    if (bind) {
-                        throw path.buildCodeFrameError('you can not have multiple v-bind directives on a single element')
-                    }
+          const _name = name.name
+          if (['vBind', 'v-bind'].indexOf(_name) < 0) return
+          bindAttribute = path
 
-                    /**
-                     * 必须使用jsx表达式
-                     * 不能使用vBind="foo" 这种字符串形式
-                     */
-                    if (!t.isJSXExpressionContainer(value)) {
-                        throw path.get('value').buildCodeFrameError('you should use JSX Expression with v-model')
-                    }
+          /**
+           * 一个节点上不能使用多次v-bind
+           */
+          if (bind) {
+            throw path.buildCodeFrameError('you can not have multiple v-bind directives on a single element')
+          }
 
-                    bind = value.expression;
-                })
+          /**
+           * 必须使用jsx表达式
+           * 不能使用vBind="foo" 这种字符串形式
+           */
+          if (!t.isJSXExpressionContainer(value)) {
+            throw path.get('value').buildCodeFrameError('you should use JSX Expression with v-model')
+          }
 
-                if (!bind) {
-                    return
-                }
+          bind = value.expression;
+        })
 
-                console.log(bindAttribute.replaceWith, 'replace')
+        if (!bind) return
 
-                bindAttribute.replaceWith(
-                    t.JSXAttribute(
-                        t.JSXIdentifier('attrs'),
-                        t.JSXExpressionContainer(bind)
-                    )
-                )
-            }
-        }
+        bindAttribute.replaceWith(
+          t.JSXAttribute(
+            t.JSXIdentifier('attrs'),
+            t.JSXExpressionContainer(bind)
+          )
+        )
+      }
     }
+  }
 }
