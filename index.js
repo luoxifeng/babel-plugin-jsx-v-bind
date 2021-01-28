371ef26718f3778da19fb4287c54a8b48d6fc86a
luoxifeng <942394682@qq.com>
2021-01-28 19:33:00 +0800

    feat: opt

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 module.exports = function (babel) {
   var t = babel.types
 
@@ -11,32 +10,37 @@ module.exports = function (babel) {
         var bind = null;
         var bindAttribute = null;
 
-        path.get('attributes').forEach(function (path) {
-          const name = path.node.name;
-          const value = path.node.value;
-          if (!name) return
+        /**
+         * 遍历开始标签上面左右的属性节点
+         * 找到v-bind
+         */
+        path.get('attributes')
+          .forEach(function (path) {
+            const name = path.node.name;
+            const value = path.node.value;
+            if (!name) return
 
-          const _name = name.name
-          if (['vBind', 'v-bind'].indexOf(_name) < 0) return
-          bindAttribute = path
+            const _name = name.name
+            if (['vBind', 'v-bind'].indexOf(_name) < 0) return
+            bindAttribute = path
 
-          /**
-           * 一个节点上不能使用多次v-bind
-           */
-          if (bind) {
-            throw path.buildCodeFrameError('you can not have multiple v-bind directives on a single element')
-          }
+            /**
+             * 一个节点上不能使用多次v-bind
+             */
+            if (bind) {
+              throw path.buildCodeFrameError('you can not have multiple v-bind directives on a single element')
+            }
 
-          /**
-           * 必须使用jsx表达式
-           * 不能使用vBind="foo" 这种字符串形式
-           */
-          if (!t.isJSXExpressionContainer(value)) {
-            throw path.get('value').buildCodeFrameError('you should use JSX Expression with v-model')
-          }
+            /**
+             * 必须使用jsx表达式
+             * 不能使用vBind="foo" 这种字符串形式
+             */
+            if (!t.isJSXExpressionContainer(value)) {
+              throw path.get('value').buildCodeFrameError('you should use JSX Expression with v-bind')
+            }
 
-          bind = value.expression;
-        })
+            bind = value.expression;
+          })
 
         if (!bind) return
 
